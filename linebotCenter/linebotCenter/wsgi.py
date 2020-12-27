8aef30ac57b7ca3bf6331a8656b5b69cb6615f18
Tarkers <44000331+tarkers@users.noreply.github.com>
2020-12-27 20:40:45 +0800

    commit

--- linebotCenter/linebotCenter/wsgi.py
+++ linebotCenter/linebotCenter/wsgi.py
@@ -2,5 +2,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling # <- 加入
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "linebotCenter.settings")
 application = Cling(get_wsgi_application()) # <- 修改
